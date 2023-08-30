00cdb08fa964e6d065b9bebbfcbdf7745fe53dd2
M-S-Raghavendra <99091196+M-S-Raghavendra@users.noreply.github.com>
2023-08-30 22:27:33 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-acount_sid = "AC4d389478e5fe840f302f91a981194771"
-auth_token = "a0ff05aeaab2b353f24118433bac3a6f"
-twilio_number = "+13157121296"
-my_number = "+919480763957"
+acount_sid = "your account sid"
+auth_token = "your auth token "
+twilio_number = "twilio number"
+my_number = "registered number"
